d26d12f320100ae89b4d17c063d61d39f5d3e553
Fahim Abrar <fahim.abrar101@gmail.com>
2025-01-12 02:18:48 +0600

    Update SkillPoint.cs

--- Assets/Scripts/Skills/SkillPoint.cs
+++ Assets/Scripts/Skills/SkillPoint.cs
@@ -1,4 +1,5 @@
 using Interfaces;
+using Interfaces.Skills;
 using UnityEngine;
 
 namespace Skills
@@ -16,20 +17,9 @@ namespace Skills
 
         public SpriteRenderer bodySpriteRenderer;
         public SpriteRenderer iconSpriteRenderer;
+        private ISkillBehavior _skillBehavior;
 
         private IIndicator indicatorInstance;
-
-        private void OnEnable()
-        {
-            // Set colors based on type
-            bodySpriteRenderer.color = skillPointType == SkillPointType.Point
-                ? new Color(1, 1, 1, 1)
-                : new Color(0, 0.3921569f, 0.3921569f, 1);
-
-            iconSpriteRenderer.color = skillPointType == SkillPointType.Point
-                ? new Color(0.6117647f, 0.427451f, 0.08627451f, 1)
-                : new Color(1, 1, 1, 1);
-        }
 
         private void OnDisable()
         {
@@ -37,9 +27,39 @@ namespace Skills
             if (indicatorInstance != null) indicatorInstance.OnDestroyMissile();
         }
 
-        public void Initialize(IIndicator indicator)
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                MyDebug.Log("SkillPoint OnTriggerEnter2D");
+                ApplySkill(other.GetComponentInParent<Plane>());
+                Destroy(gameObject);
+            }
+        }
+
+        // private void SetSillGUI()
+        // {
+        //     // Set colors based on type
+        //     bodySpriteRenderer.color = skillPointType == SkillPointType.Point
+        //         ? new Color(1, 1, 1, 1)
+        //         : new Color(0, 0.3921569f, 0.3921569f, 1);
+        //
+        //     iconSpriteRenderer.color = skillPointType == SkillPointType.Point
+        //         ? new Color(0.6117647f, 0.427451f, 0.08627451f, 1)
+        //         : new Color(1, 1, 1, 1);
+        // }
+
+        public void Initialize(IIndicator indicator, ISkillBehavior skillBehavior)
         {
             indicatorInstance = indicator;
+            _skillBehavior = skillBehavior;
+            // SetSillGUI();
+        }
+
+
+        public void ApplySkill(Plane plane)
+        {
+            _skillBehavior.ApplyEffect(plane);
         }
     }
 }
