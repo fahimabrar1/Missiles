63dca04724289c778c8f5770dfa3cc107b225a36
Fahim Abrar <fahim.abrar101@gmail.com>
2025-01-11 19:48:48 +0600

    Added Destroy effect

--- Assets/Scripts/Plane/Plane.cs
+++ Assets/Scripts/Plane/Plane.cs
@@ -1,3 +1,4 @@
+using DefaultNamespace;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -6,12 +7,15 @@ public class Plane : MonoBehaviour
     public float speed = 30f;
     public float speedMultiplier = 2f;
     public float steer = 20f;
-    public float tiltAmount = 15f; 
-    private Rigidbody2D _rb;
+    public float tiltAmount = 15f;
+    public GameObject DestroyParticle;
+    public AudioSource DestroySound;
+
     private PlayerInput _playerInput;
+    private Rigidbody2D _rb;
 
     /// <summary>
-    /// Awake is called when the script instance is being loaded.
+    ///     Awake is called when the script instance is being loaded.
     /// </summary>
     private void Awake()
     {
@@ -20,25 +24,39 @@ public class Plane : MonoBehaviour
     }
 
     /// <summary>
-    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    ///     This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     private void FixedUpdate()
     {
         var input = GetInput();
-    
+
         // Apply forward movement
         _rb.velocity = speed * speedMultiplier * Time.deltaTime * transform.up;
-       
+
         // Calculate the rotation based on horizontal input
         var rotation = -input.x * steer * Time.deltaTime;
 
         // Update the plane's overall rotation
         var currentRotation = transform.rotation;
-        
+
         // Apply tilt for visual effect
         var tilt = -input.x * tiltAmount; // Negative for correct tilt direction
-        var tiltRotation = Quaternion.Euler(0,tilt,currentRotation.eulerAngles.z + rotation);
+        var tiltRotation = Quaternion.Euler(0, tilt, currentRotation.eulerAngles.z + rotation);
         transform.rotation = tiltRotation;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Missile")) return;
+
+        // Trigger explosion effect
+        if (DestroyParticle != null) Instantiate(DestroyParticle, transform.position, Quaternion.identity);
+
+        // Play explosion sound
+        if (DestroySound != null) LevelAudioPlayer.instance.OnPlayAudioByName("explosion-large");
+        GameManager.Instance.OnGameOver();
+        Destroy(gameObject);
     }
 
     private Vector2 GetInput()
