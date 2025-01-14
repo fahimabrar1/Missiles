2dc41b851d8542f265f1eec9ba972fcc904d6cdc
Fahim Abrar <fahim.abrar101@gmail.com>
2025-01-15 02:25:59 +0600

    Implemented Sound System

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 public class Settings : MonoBehaviour
 {
     public ToggleButton audioToggle;
+    private AudioListener AudioListener;
+
+    private void Awake()
+    {
+        AudioListener = FindObjectOfType<AudioListener>();
+    }
 
     private void OnEnable()
     {
         var isAudioOn = PlayerPrefs.GetInt("audio_on", 1);
 
         audioToggle.ToggleButtonOn(isAudioOn == 1);
+        AudioListener.enabled = isAudioOn == 1;
     }
 
 
     public void OnChangeSound(bool value)
     {
+        AudioListener.enabled = value;
         PlayerPrefs.SetInt("audio_on", value ? 1 : 0);
         PlayerPrefs.Save();
     }
